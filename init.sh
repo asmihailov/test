17930dd59aa5cff04058ef53548cf03f96109b1a
1 <1>
2019-10-20 23:38:35 +0000

    3 done

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 #sudo ln -sf etc/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
 #sudo unlink /etc/nginx/sites-enabled/default
-#sudo cp etc/nginx.conf /etc/nginx/sites-available/
-#sudo ln -sf /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/nginx.conf
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app &
-sudo gunicorn -c /home/box/web/etc/guni.py ask.wsgi:app &
+sudo cp etc/nginx.conf /etc/nginx/sites-available/nginx.conf
+sudo ln -sf /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+#sudo gunicorn -c /home/box/web/etc/hello.py hello:app &
+sudo gunicorn --bind='0.0.0.0:8000' --pythonpath /home/box/web/ask ask.wsgi:application &
