693251db310cd5d3a4db0320ecd9d018e2212628
q <1>
2019-10-28 22:30:57 +0000

    not ready 3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,24 @@
-from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User 
 
-class Question(django.contrib.auth.models.User):
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateTimeField();
+class Question(models.Model):
+	title = models.CharField(max_length=50)
+	text = models.TextField(max_length=100)
+	added_at = models.DateTimeField(blank = True, auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.CharField(max_length=20)
+	likes = models.ForeignKey(User, null=True)
+	def __unicode__(self):
+		return self.title
+class Answer(models.Model):
+	text = models.CharField(max_length=100)
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, null=True)
+	author = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	def __unicode__(self):
+		return self.text
+	class QuestionManager(models.Manager):
+		def new(self):
+			return self.order_by('-added_at')
+		def popular(self):
+			return self.order_by('-rating')
--- my.sh
+++ my.sh
@@ -1,6 +1,6 @@
 sudo /etc//init.d/mysql start
 sudo mysql -uroot -e "create database qa character set utf8;"
-mysql -uroot -e "grant all privileges on qa.* to 'box'@'localhost' with grant option;"
+mysql -uroot -e "grant all privileges on *.* to 'root'@'localhost' with grant option;"
 /home/box/web/ask/manage.py makemigrations
 /home/box/web/ask/manage.py migrate
 
